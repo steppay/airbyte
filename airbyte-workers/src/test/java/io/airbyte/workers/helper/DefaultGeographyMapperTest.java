0803ce9d9ad6410b9397d83c3c101102d2b09305
pmossman <parker@airbyte.io>
2022-10-12 13:44:51 -0700

    pmd

--- airbyte-workers/src/test/java/io/airbyte/workers/helper/DefaultGeographyMapperTest.java
+++ airbyte-workers/src/test/java/io/airbyte/workers/helper/DefaultGeographyMapperTest.java
@@ -12,7 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
-public class DefaultGeographyMapperTest {
+class DefaultGeographyMapperTest {
 
   @Test
   void testGetTaskQueue() {
