fa5c60dc1b2fa0de1038df26cb4e4e21d5548ddb
Mark Berger <m19.berger@gmail.com>
2022-10-12 17:49:47 +0300

    Update Notification Settings design
    - Removed styled-components
    - Added new behavior 'Test' and 'Submit' buttons

--- airbyte-webapp/src/pages/SettingsPage/pages/NotificationPage/components/WebHookForm.tsx
+++ airbyte-webapp/src/pages/SettingsPage/pages/NotificationPage/components/WebHookForm.tsx
@@ -1,51 +1,52 @@
 import { Field, FieldProps, Form, Formik } from "formik";
 import React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
-import styled from "styled-components";
+// import styled from "styled-components";
 import * as yup from "yup";
 
 import { Label, LabeledSwitch } from "components";
 import { Row, Cell } from "components/SimpleTableComponents";
 import { Button } from "components/ui/Button";
 import { Input } from "components/ui/Input";
+import { Tooltip } from "components/ui/Tooltip";
 
 import { WebhookPayload } from "hooks/services/useWorkspace";
 import { equal } from "utils/objects";
 
-const Text = styled.div`
-  font-style: normal;
-  font-weight: normal;
-  font-size: 13px;
-  line-height: 150%;
-  padding-bottom: 5px;
-`;
+// const Text = styled.div`
+//   font-style: normal;
+//   font-weight: normal;
+//   font-size: 13px;
+//   line-height: 150%;
+//   padding-bottom: 5px;
+// `;
 
-const InputRow = styled(Row)`
-  height: auto;
-  margin-bottom: 40px;
-`;
+// const InputRow = styled(Row)`
+//   height: auto;
+//   margin-bottom: 40px;
+// `;
 
-const Message = styled(Text)`
-  margin: -40px 0 21px;
-  padding: 0;
-  color: ${({ theme }) => theme.greyColor40};
-`;
+// const Message = styled(Text)`
+//   margin: -40px 0 21px;
+//   padding: 0;
+//   color: ${({ theme }) => theme.greyColor40};
+// `;
 
-const FeedbackCell = styled(Cell)`
-  &:last-child {
-    text-align: left;
-  }
-  padding-left: 11px;
-`;
+// const FeedbackCell = styled(Cell)`
+//   &:last-child {
+//     text-align: left;
+//   }
+//   padding-left: 11px;
+// `;
 
-const Success = styled.div`
-  font-size: 13px;
-  color: ${({ theme }) => theme.successColor};
-`;
-
-const Error = styled(Success)`
-  color: ${({ theme }) => theme.dangerColor};
-`;
+// const Success = styled.div`
+//   font-size: 13px;
+//   color: ${({ theme }) => theme.successColor};
+// `;
+//
+// const Error = styled(Success)`
+//   color: ${({ theme }) => theme.dangerColor};
+// `;
 
 const webhookValidationSchema = yup.object().shape({
   webhook: yup.string().url("form.url.error"),
@@ -65,28 +66,24 @@ const WebHookForm: React.FC<WebHookFormProps> = ({ webhook, onSubmit, successMes
   const { formatMessage } = useIntl();
 
   const feedBackBlock = (dirty: boolean, isSubmitting: boolean, webhook?: string) => {
+    console.log("isSubmitting", isSubmitting);
+    console.log("webhook", webhook);
+    console.log("dirty", dirty);
+
     if (successMessage) {
-      return <Success>{successMessage}</Success>;
+      return <div>{successMessage}</div>;
     }
 
     if (errorMessage) {
-      return <Error>{errorMessage}</Error>;
+      return <div>{errorMessage}</div>;
     }
 
     if (dirty) {
-      return (
-        <Button isLoading={isSubmitting} type="submit">
-          <FormattedMessage id="form.saveChanges" />
-        </Button>
-      );
+      return null;
     }
 
-    if (webhook) {
-      return (
-        <Button isLoading={isSubmitting} type="submit">
-          <FormattedMessage id="settings.test" />
-        </Button>
-      );
+    if (webhook && isSubmitting) {
+      return null;
     }
 
     return null;
@@ -115,10 +112,10 @@ const WebHookForm: React.FC<WebHookFormProps> = ({ webhook, onSubmit, successMes
           >
             <FormattedMessage id="settings.webhookTitle" />
           </Label>
-          <Text>
-            <FormattedMessage id="settings.webhookDescriprion" />
-          </Text>
-          <InputRow>
+          {/* <Text> */}
+          {/*   <FormattedMessage id="settings.webhookDescription" /> */}
+          {/* </Text> */}
+          <Row>
             <Cell flex={3}>
               <Field name="webhook">
                 {({ field, meta }: FieldProps<string>) => (
@@ -132,14 +129,28 @@ const WebHookForm: React.FC<WebHookFormProps> = ({ webhook, onSubmit, successMes
                 )}
               </Field>
             </Cell>
-            <FeedbackCell>{feedBackBlock(dirty, isSubmitting, initialValues.webhook)}</FeedbackCell>
-          </InputRow>
-          {initialValues.webhook ? (
-            <Message>
-              <FormattedMessage id="settings.webhookTestText" />
-            </Message>
-          ) : null}
-          <InputRow>
+            {/* <Cell>{feedBackBlock(dirty, isSubmitting, initialValues.webhook)}</Cell> */}
+            <Cell>
+              <Tooltip
+                placement="top"
+                disabled={!!initialValues.webhook || !!errors.webhook}
+                control={
+                  <Button isLoading={isSubmitting} variant="secondary" type="submit">
+                    <FormattedMessage id="settings.test" />
+                  </Button>
+                }
+              >
+                <FormattedMessage id="settings.webhookTestText" />
+              </Tooltip>
+            </Cell>
+            {feedBackBlock(dirty, isSubmitting, initialValues.webhook)}
+          </Row>
+          {/* {initialValues.webhook ? ( */}
+          {/*   <Text> */}
+          {/*     <FormattedMessage id="settings.webhookTestText" /> */}
+          {/*   </Text> */}
+          {/* ) : null} */}
+          <Row>
             <Cell flex={1}>
               <Field name="sendOnFailure">
                 {({ field }: FieldProps<boolean>) => (
@@ -164,7 +175,10 @@ const WebHookForm: React.FC<WebHookFormProps> = ({ webhook, onSubmit, successMes
                 )}
               </Field>
             </Cell>
-          </InputRow>
+          </Row>
+          <Button isLoading={isSubmitting} type="submit">
+            <FormattedMessage id="form.saveChanges" />
+          </Button>
         </Form>
       )}
     </Formik>
